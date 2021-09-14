f83fcb0d84a8a911cc56d90650c1f485056c6868
woodser <woodser@protonmail.com>
2021-09-14 08:27:45 -0400

    switch api to use typescript auto-generated from protobuf using grpc-web (#11)
    haveno daemon can get payment accounts and the user's created offers

--- bin/build_protobuf.sh
+++ bin/build_protobuf.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+// generate imports for haveno services and types using grpc-web
 cd ./config || exit 1
-protoc -I=./ *.proto --js_out=import_style=commonjs:./ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./ || exit 1
+protoc -I=./ *.proto --js_out=import_style=commonjs,binary:./ --grpc-web_out=import_style=typescript,mode=grpcwebtext:./ || exit 1
 cd ../
-mv ./config/grpc_grpc_web_pb.js ./src || exit 1
-mv ./config/grpc_pb.js ./src || exit 1
-mv ./config/pb_pb.js ./src || exit 1
+
+// move imports to src folder
+mv ./config/grpc_pb.d.ts ./src/protobuf || exit 1
+mv ./config/grpc_pb.js ./src/protobuf || exit 1
+mv ./config/pb_pb.d.ts ./src/protobuf || exit 1
+mv ./config/pb_pb.js ./src/protobuf || exit 1
+mv ./config/GrpcServiceClientPb.ts ./src/protobuf || exit 1
