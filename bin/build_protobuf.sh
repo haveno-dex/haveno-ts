3d8e37e64604cdb49b6d9d18adeec5815e5dc6ce
woodser <woodser@protonmail.com>
2021-09-15 08:06:50 -0400

    use underscores in grpc.proto for correct camelcase in grpc-web
    fix possibly undefined typescript checks
    rebuild protobuf TS definitions

--- bin/build_protobuf.sh
+++ bin/build_protobuf.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-// generate imports for haveno services and types using grpc-web
+# generate imports for haveno services and types using grpc-web
 cd ./config || exit 1
 protoc -I=./ *.proto --js_out=import_style=commonjs,binary:./ --grpc-web_out=import_style=typescript,mode=grpcwebtext:./ || exit 1
 cd ../
 
-// move imports to src folder
+# move imports to src folder
 mv ./config/grpc_pb.d.ts ./src/protobuf || exit 1
 mv ./config/grpc_pb.js ./src/protobuf || exit 1
 mv ./config/pb_pb.d.ts ./src/protobuf || exit 1
