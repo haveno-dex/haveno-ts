ebe0726356f2e1fe103beaee28de5403a26c61e5
woodser <woodser@protonmail.com>
2021-12-08 06:22:36 -0500

    add ability to start haveno processes from tests
    test making or taking offer with insufficient unlocked balance

---
+++ src/HavenoUtils.ts
@@ -0,0 +1,42 @@
+const assert = require("assert");
+const console = require('console');
+
+/**
+ * Collection of utilities for working with Haveno.
+ */
+class HavenoUtils {
+    
+  static LOG_LEVEL = 0;
+    
+  /**
+   * Log a message.
+   *
+   * @param {int} level - log level of the message
+   * @param {string} msg - message to log
+   */
+  static log(level: number, msg: string) {
+    assert(level === parseInt(level + "", 10) && level >= 0, "Log level must be an integer >= 0");
+    if (HavenoUtils.LOG_LEVEL >= level) process.stdout.write(msg);
+  }
+  
+  /**
+   * Set the log level with 0 being least verbose.
+   *
+   * @param {int} level - the log level
+   */
+  static async setLogLevel(level: number) {
+    assert(level === parseInt(level + "", 10) && level >= 0, "Log level must be an integer >= 0");
+    HavenoUtils.LOG_LEVEL = level;
+  }
+  
+  /**
+   * Get the log level.
+   *
+   * @return {int} the current log level
+   */
+  static getLogLevel(): number {
+    return HavenoUtils.LOG_LEVEL;
+  }
+}
+
+export {HavenoUtils};
