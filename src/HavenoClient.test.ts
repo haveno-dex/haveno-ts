abd4b1dd29f1fccbca9253ce760342a2f48efa95
woodser <woodser@protonmail.com>
2023-02-16 10:54:44 -0500

    Test 0c4ce623a7bbe4b4788224bd2087875b395d623c

--- src/HavenoClient.test.ts
+++ src/HavenoClient.test.ts
@@ -362,7 +362,7 @@ afterAll(async () => {
 // ----------------------------------- TESTS ----------------------------------
 
 test("Can get the version (ZI)", async () => {
-  console.log("Testing from 88f0ad543a2e67db6b7d399962b1061bf52fcaa0 2");
+  console.log("Testing from 0c4ce623a7bbe4b4788224bd2087875b395d623c 2");
   const version = await arbitrator.getVersion();
   expect(version).toEqual(TestConfig.haveno.version);
 });
