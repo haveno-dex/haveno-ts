d12b552290173d9eef7d93f0816d4a33335bef84
woodser <woodser@protonmail.com>
2024-02-19 07:43:10 -0500

    bump havenod version to 1.0.18

--- src/HavenoClient.test.ts
+++ src/HavenoClient.test.ts
@@ -334,7 +334,7 @@ const TestConfig = {
     testDataDir: "./testdata",
     haveno: {
         path: "../haveno",
-        version: "1.0.17"
+        version: "1.0.18"
     },
     monerod: {
         url: "http://localhost:" + getNetworkStartPort() + "8081", // 18081, 28081, 38081 for mainnet, testnet, and stagenet, respectively
