4be355bc258e00b6b92c4925c38950e9a1da7088
woodser <woodser@protonmail.com>
2024-05-21 18:00:50 -0400

    test havenod 1.0.6

--- src/HavenoClient.test.ts
+++ src/HavenoClient.test.ts
@@ -355,7 +355,7 @@ const TestConfig = {
     testDataDir: "./testdata",
     haveno: {
         path: "../haveno",
-        version: "1.0.4"
+        version: "1.0.6"
     },
     monerod: {
         url: "http://localhost:" + getNetworkStartPort() + "8081", // 18081, 28081, 38081 for mainnet, testnet, and stagenet, respectively
