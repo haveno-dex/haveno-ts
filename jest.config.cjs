a167849035a410bdcd98d896eec7020efb8cef6f
mainnet-pat <mainnet_pat@protonmail.com>
2024-08-28 20:24:00 +0000

    Update to new monero-ts, upgrade jest

---
+++ jest.config.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  rootDir: "./",
+  preset: "ts-jest/presets/default-esm",
+  resolver: "ts-jest-resolver",
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts}",
+    "!**/node_modules/**",
+  ],
+  coveragePathIgnorePatterns: [
+    ".*/src/.*\\.d\\.ts",
+    ".*/src/.*\\.test\\.{ts,js}",
+    ".*/src/.*\\.test\\.headless\\.js",
+  ],
+  roots: [
+    "<rootDir>/src",
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testEnvironment: "node",
+  verbose: true,
+  maxConcurrency: 1,
+  testTimeout: 180000,
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+  ],
+  globals: {
+    window: {},
+    XMLHttpRequest: require("node-xmlhttprequest").XMLHttpRequest,
+  },
+};
