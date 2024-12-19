c0fff3b95ad85a80025f7f58626807875f855013
im-adithya <imadithyavardhan@gmail.com>
2024-12-19 16:15:21 +0530

    chore: add xhr2 package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import XMLHttpRequest from "xhr2";
+global.XMLHttpRequest = XMLHttpRequest;
+
 import HavenoClient from "./HavenoClient";
 import HavenoError from "./types/HavenoError";
 import HavenoUtils from "./utils/HavenoUtils";
