0dda005f96a3c6152915330d360232ab274996c4
woodser <woodser@protonmail.com>
2024-12-21 09:20:08 -0500

    test havenod 1.0.17

--- src/HavenoClient.test.ts
+++ src/HavenoClient.test.ts
@@ -366,7 +366,7 @@ const TestConfig = {
     deferralMs: 25000,
     haveno: {
         path: "../haveno",
-        version: "1.0.16"
+        version: "1.0.17"
     },
     monerod: {
         url: "http://localhost:" + getNetworkStartPort() + "8081", // 18081, 28081, 38081 for mainnet, testnet, and stagenet, respectively
