c2030e7cfc4da03a76347eb7c8dbd6eb4c0496ae
woodser <13068859+woodser@users.noreply.github.com>
2026-06-26 20:46:32 -0400

    test satispay over grpc api

--- src/HavenoClient.test.ts
+++ src/HavenoClient.test.ts
@@ -5241,6 +5241,12 @@ function testPaymentAccount(account: PaymentAccount, form: PaymentAccountForm) {
         expect(account.getTradeCurrenciesList().length).toEqual(1);
         expect(account.getTradeCurrenciesList()[0].getCode()).toEqual("INR");
         break;
+    case PaymentAccountForm.FormId.SATISPAY:
+        expect(account.getPaymentAccountPayload()!.getCountryBasedPaymentAccountPayload()!.getSatispayAccountPayload()!.getHolderName()).toEqual(getFormField(form, PaymentAccountFormField.FieldId.HOLDER_NAME).getValue());
+        expect(account.getPaymentAccountPayload()!.getCountryBasedPaymentAccountPayload()!.getSatispayAccountPayload()!.getMobileNr()).toEqual(getFormField(form, PaymentAccountFormField.FieldId.MOBILE_NR).getValue());
+        expect(account.getTradeCurrenciesList().length).toEqual(1);
+        expect(account.getTradeCurrenciesList()[0].getCode()).toEqual("EUR");
+        break;
     case PaymentAccountForm.FormId.JAPAN_BANK: {
         const bank = getFormField(form, PaymentAccountFormField.FieldId.BANK_NAME).getValue(); // "<code> <ja name> [<en name>]"
         expect(account.getPaymentAccountPayload()!.getJapanBankAccountPayload()!.getBankCode()).toEqual(bank.substring(0, 4)); // code derived from selection
